667d13ae969de299df3a43a741f363eb13efeeb1
Dharmraj01 <103097446+Dharmraj01@users.noreply.github.com>
2022-04-24 09:50:38 +0530

    Add files via upload

---
+++ pra2Q5.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter the value ";
+	cin>>a;
+	if(a == 2)
+	{
+		cout<<a<<" is prime";
+	}
+	else if(a % 2 == 0)
+	{
+		cout<<a<<" is not prime";
+	}
+	else 
+	{
+		cout<<a<<" is prime";
+		}
+}
